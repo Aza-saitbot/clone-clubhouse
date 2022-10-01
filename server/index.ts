14769722e13fbdebcf90832aaffe63f7a78c8d4b
Saitbot <73251805+Aza-saitbot@users.noreply.github.com>
2022-10-01 10:46:05 +0300

    fist commit v1

---
+++ server/index.ts
@@ -0,0 +1,61 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import multer from 'multer'
+import {nanoid} from 'nanoid'
+import sharp from 'sharp'
+import fs from 'fs'
+
+dotenv.config({
+    path:'server/.env'
+})
+
+import '../server/core/db'
+
+import { passport } from './core/passport';
+
+const app=express()
+
+const uploader=multer({
+    storage:multer.diskStorage({
+        destination:function (_, __, cb) {
+            cb(null,'public/avatars')
+        },
+        filename:function (_,file,cb){
+            cb(null,file.fieldname + '-'+nanoid(6) + "."+ file.mimetype.split('/').pop())
+        }
+    })
+})
+
+app.use(cors());
+app.use(passport.initialize());
+
+app.post('/upload', uploader.single('photo'),(req,res)=>{
+    const filePath=req.file.path
+    sharp(filePath)
+        .resize(150,150)
+        .toFormat('jpeg')
+        .toFile(filePath.replace('.png','.jpeg'),(err)=>{
+        if (err){
+            throw err;
+        }
+        fs.unlinkSync(filePath)
+        res.json({
+            url:`avatars/${req.file.filename.replace('.png','.jpeg')}`
+        })
+    })
+})
+
+app.get('/auth/github',
+    passport.authenticate('github'));
+
+app.get('/auth/github/callback',
+    passport.authenticate('github', { failureRedirect: '/login' }),
+    (req, res)=> {
+        res.send(`<script>window.opener.postMessage('${JSON.stringify(req.user,)}',"*");window.close()</script>`)
+    });
+
+
+
+
+app.listen(3001,()=>console.log('SERVER RUNNED!'))
